4382a4dd2b22856699dffe26ebc8621c67fad131
JacyCui <jiacaicui@163.com>
2022-09-12 15:11:23 +0800

    01-intro

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,90 @@
+module.exports = {
+    title: 'Hello VuePress', // 网站标题
+    description: 'Just playing around', // 网站描述
+
+    // 插件
+    plugins: [
+        ['@maginapp/vuepress-plugin-katex', { // katex公式
+          delimiters: 'dollars'
+        }],
+
+        ['vuepress-plugin-container', {
+            type: 'definition',
+            before: info => `<div class="definition"><p class="title">${info}</p>`,
+            after: '</div>',
+        }],
+
+        ['vuepress-plugin-container', {
+            type: 'theorem',
+            before: info => `<div class="theorem"><p class="title">${info}</p>`,
+            after: '</div>',
+        }],
+
+        ['vuepress-plugin-container', {
+            type: 'conclusion',
+            before: info => `<div class="conclusion"><p class="title">${info}</p>`,
+            after: '</div>',
+        }],
+    ],
+    
+    markdown: { // markdown渲染设置
+        lineNumbers: true
+    },
+    locales: {  // 网站语言设置
+        '/': {
+            lang: 'zh-CN',
+            title: 'VuePress',
+            description: 'Vue-powered Static Site Generator'
+        }
+    },
+    themeConfig: { // 主题设置
+        // 关于导航栏
+        logo: '/favicon.png', // 导航栏logo
+        navbar: true, // 启用导航栏
+        nav: [ // 导航栏内容设置
+            {
+                text: '目录',
+                items: [
+                    {text: '1 引论', link: '/01-intro/'}
+                ]
+            }
+            // { text: '主页', link: '/' },
+            // { text: 'Demo', link: '/demo/' },
+            // { text: 'lalala', link: '/lalala/' },
+            // { text: 'External', link: 'https://google.com' },
+            // {
+            //     text: 'Languages',
+            //     ariaLabel: 'Language Menu',
+            //     items: [
+            //       { text: 'Chinese', link: '/language/chinese/' },
+            //       { text: 'Japanese', link: '/language/japanese/' }
+            //     ]
+            // }
+        ],
+        repo: 'vuejs/vuepress', // 文档项目的github仓库
+
+        // 关于侧边栏
+        displayAllHeaders: false, // 显示所有页面的标题链接，否则只显示当前页面的
+        activeHeaderLinks: false, // 活动的标题链接
+        sidebarDepth: 2, // 
+        sidebar: [
+            '/01-intro/',
+            // ['/demo', 'Explicit link text'], // 显示地指定文字
+        ],
+
+        // 关于页脚
+        nextLinks: true, // 下一篇
+        prevLinks: true, // 上一篇
+        lastUpdated: '最后更新', // string | boolean 最后更新时间
+        repoLabel: '查看源码',
+        // docsRepo: 'vuejs/vuepress', // 文档仓库，默认为项目仓库
+        // docsDir: 'docs', // 文档目录
+        // docsBranch: 'master', // 文档分支
+        editLinks: true,
+        // editLinkText: '帮助我们改善此页面！'
+
+
+        smoothScroll: true, // 页面滚动 
+    }
+}
+
---
+++ docs/README.md
@@ -0,0 +1,25 @@
+---
+home: true # 主页
+heroImage: /static-analysis.png # 主页大图
+heroText: 静态分析 # 主页标题
+tagline: 基于南京大学软件分析课程的静态分析基础教程 # 主页副标题
+actionText: 开始学习 → # 主页按钮文字
+actionLink: /01-intro/ # 主页按钮链接
+# navbar: true # 启用导航栏
+features: # 特点介绍
+- title: 内容全面
+  details: 涵盖一个自顶向下，然后又自底向上的完整的静态分析体系。
+- title: 结构严谨
+  details: 基于集合论与数理逻辑来阐释关键概念与性质。
+- title: 通俗易懂
+  details: 由于本身就是笔者从学生视角作的笔记形成的教程，对初学者更加友好。
+# footer: MIT Licensed | Copyright © 2018-present Evan You # 页脚
+---
+
+<!-- 使用markdown插槽来设置页脚 -->
+
+::: slot footer
+<a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/"><img alt="知识共享许可协议" style="border-width:0" src="https://i.creativecommons.org/l/by-nc-sa/4.0/88x31.png" /></a><br />本作品采用<a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/">知识共享署名-非商业性使用-相同方式共享 4.0 国际许可协议</a>进行许可。
+:::
+
+
